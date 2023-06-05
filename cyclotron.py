41547646b76f343ca99bb46aadbf5a1570954a06
Nigel Shen <1240234313@qq.com>
2023-06-05 15:20:18 +0800

    Create cyclotron.py

---
+++ cyclotron.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as sp
+import finufft
+import matplotlib.animation as animation
+fig, ax = plt.subplots()
+artist = []
+
+L = 1
+N = 40000
+NG = 128
+DT = 0.0005 # Time step size
+QM = -1
+WP = 1  # omega p
+VT = 1  # Thermal Velocity
+NT = int(10 // DT)  # number of time steps
+lambdaD = VT / WP
+Q = -60 / N  # Charge of a particle
+# self.rho_back = - self.Q * self.N / self.L  # background rho
+dx = L / NG
+B0 = 300
+
+sigmas = np.array([[1/30],[1/10]]) / np.sqrt(2)
+XP = np.random.randn(2, N) * sigmas
+VP = np.random.randn(2, N)
+# plt.scatter(XP[0,:], XP[1,:], alpha=0.1)
+# plt.xlim([-0.5, 0.5])
+# plt.ylim([-0.5, 0.5])
+# plt.show()
+
+# Prepare for convolution kernel:
+extension = 8
+wm = np.linspace(- NG * np.pi / L, NG * np.pi / L, extension*NG, endpoint=False) ## 8 times finer than regular Fourier step
+wm1, wm2 = np.meshgrid(wm, wm)
+s = np.sqrt(wm1**2 + wm2**2)
+
+## Construct mollified Green's function
+LT = 1.5 * L ## Truncation window size
+green = (1-sp.jv(0, LT*s)) / (s**2) - (LT*np.log(LT)*sp.jv(1, LT*s)) / s ## Green function in spectral space
+green[extension*NG//2, extension*NG//2] = (LT**2/4 - LT**2*np.log(LT)/2)
+
+r = L / NG
+J = np.fft.fftshift(wm) * np.ones([NG * extension, 1])
+Kabsolute = np.transpose(np.sqrt(J**2 + np.transpose(J)**2))
+Kabsolute[0,0] = 1  # avoid 0 on denominator
+Shat = (2 * sp.j1(r * Kabsolute) / (r * Kabsolute)) ** 2 
+Shat[0, 0] = 1
+Shat = Shat / (r **2)
+
+green = Shat * np.fft.fftshift(green)
+# green = np.fft.fftshift(green)
+## Precomputation
+T1 = np.fft.ifftshift(np.fft.ifft2(green)) # * deltahat
+T = T1[extension*NG//4:extension*NG*3//4, extension*NG//4:extension*NG*3//4]
+T = np.fft.fft2(T)
+
+for clock in range(NT):
+    '''
+    1. Solve the electric field using Vico-Greengard and evaluate at particle positions using NUFFT
+    
+    2. Push the particles using leapfrog
+    '''
+    # NUFFT Type 1 to evaluate exp(-ikX)
+    raw = finufft.nufft2d1(XP[0, :] * np.pi / (2*L), XP[1, :] * np.pi / (2*L), 0j + np.ones(N), (4*NG, 4*NG), eps=1E-14, modeord=1)
+    
+    # Compute Electric Field
+    phi_Hat = Q * T * np.conjugate(raw) / (16*NG**2)
+    E0 = phi_Hat * -1j * np.transpose(J)[::2, ::2]
+    E1 = phi_Hat * -1j * J[::2, ::2]
+
+    # Compute Acceleration due to Electric Field
+    coeff1 = np.conjugate(E0 * Shat[::2, ::2])
+    a1 = np.real(finufft.nufft2d2(XP[0, :] * np.pi / (2*L) + np.pi, XP[1, :] * np.pi / (2*L) + np.pi, coeff1, eps=1e-14, modeord=1) * QM) / (NG * 4) **2
+    coeff2 = np.conjugate(E1 * Shat[::2, ::2])
+    a2 = np.real(finufft.nufft2d2(XP[0, :] * np.pi / (2*L) + np.pi, XP[1, :] * np.pi / (2*L) + np.pi, coeff2, eps=1e-14, modeord=1) * QM) / (NG * 4) **2
+
+    # Compute Acceleration due to Magnetic Field
+    # Leapfrog
+    if not clock==0:
+        VP = VP + DT * np.array([a1, a2]) 
+        new_VP1 = (QM * B0*DT/2 *(VP[1,:]+DT*(a2-QM*B0*VP[0,:]/2)) + VP[0,:] + DT * (a1+QM*B0*VP[1,:]/2)) / (1+(QM*B0*DT/2)**2)
+        new_VP2 = (-QM * B0*DT/2 *(VP[0,:]+DT*(a1+QM*B0*VP[1,:]/2)) + VP[1,:] + DT * (a2-QM*B0*VP[0,:]/2)) / (1+(QM*B0*DT/2)**2)
+        VP = np.array([new_VP1, new_VP2])
+    else:
+        a1 = a1 + QM * B0 * VP[1,:]
+        a2 = a2 - QM * B0 * VP[0,:]
+        VP = VP + DT * np.array([a1, a2]) / 2
+    XP = XP + DT * VP
+
+    if clock%200==0:
+        print(clock)
+        rho_Hat = -np.conjugate(raw) * Shat[::2,::2] * Q
+        rho = np.fft.fftshift(np.real(np.fft.ifft2(rho_Hat)))[int(1.5*NG):int(2.5*NG), int(1.5*NG):int(2.5*NG)]
+        container = ax.imshow(rho, vmin=0,  vmax = 60*300 / np.pi)
+        #fig.colorbar(container)
+        artist.append([container])
+
+ani = animation.ArtistAnimation(fig=fig, artists=artist, interval=40)
+ani.save(filename="cyclotron.mp4", writer="ffmpeg")
